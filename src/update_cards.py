e51d006581e40a9bd66423b6810d2f7a0a1bcced
eoinc836 <eoinc836@gmail.com>
2025-05-11 23:07:34 +0100

    Card update

---
+++ src/update_cards.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+
+# A GET request to the API
+response = requests.get(url)
+
+json_object = json.dumps(response.json(), indent=4)
+
+with open("data.json", "w") as outfile:
+    outfile.write(json_object)
